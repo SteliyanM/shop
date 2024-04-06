2baa993d3d70584b5711a11ce4840644f45806f2
SteliyanM <proto711bg@gmail.com>
2024-04-06 17:01:31 +0300

    Added remove product from shopping cart and design for shopping cart

--- bokatas_store/orders/models.py
+++ bokatas_store/orders/models.py
@@ -1,10 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from bokatas_store.accounts.mixins import UserField
 from bokatas_store.products.models import Product
 
 
-class Order(UserField):
+UserModel = get_user_model()
+
+
+class Order(models.Model):
     total_price = models.DecimalField(
         max_digits=12,
         decimal_places=2,
@@ -21,4 +25,9 @@ class Order(UserField):
     products = models.ManyToManyField(
         Product,
         related_name="orders",
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
     )
--- bokatas_store/shopping_cart/urls.py
+++ bokatas_store/shopping_cart/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from .views import DetailsShoppingCartView, add_product_to_shoppingcart
+from django.urls import path, include
+from .views import DetailsShoppingCartView, add_product_to_shoppingcart_and_order, remove_product_to_shoppingcart_and_order
 
 
 urlpatterns = (
     path("", DetailsShoppingCartView.as_view(), name="details-shopping-cart"),
-    path("add/<int:pk>/", add_product_to_shoppingcart, name="add-product-to-shopping-cart"),
+    path("<int:pk>/", include([
+        path("add/", add_product_to_shoppingcart_and_order, name="add-product-to-shopping-cart"),
+        path("remove/", remove_product_to_shoppingcart_and_order, name="remove-product-to-shopping-cart"),
+    ])),
 )
