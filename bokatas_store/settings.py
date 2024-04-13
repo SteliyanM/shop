bcf1984f4a2c9e4212f8ae44b2a8e035aa22a07b
SteliyanM <proto711bg@gmail.com>
2024-04-13 17:31:58 +0300

    Deployment

--- bokatas_store/settings.py
+++ bokatas_store/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = 'bokatas_store.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "store-db",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "djangostore-server.postgres.database.azure.com",
+        "PORT": "5432",
     }
 }
 
